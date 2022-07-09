32ed59133517efe883a51717f051f3713ae4361a
Jens Maus <mail@jens-maus.de>
2022-07-09 21:52:16 +0200

    added ssdpd user

--- buildroot-external/package/ssdpd/ssdpd.mk
+++ buildroot-external/package/ssdpd/ssdpd.mk
@@ -8,6 +8,10 @@ SSDPD_VERSION = 1.0.0
 SSDPD_SOURCE =
 SSDPD_LICENSE = Apache-2.0
 
+define SSDPD_USERS
+	ssdpd -1 ssdpd -1 * - - - SSDP daemon
+endef
+
 define SSDPD_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 0755 $(SSDPD_PKGDIR)/S50ssdpd \
 		$(TARGET_DIR)/etc/init.d/S50ssdpd
